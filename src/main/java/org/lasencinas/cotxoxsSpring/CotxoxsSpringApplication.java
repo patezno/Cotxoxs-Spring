67440c38577f815d2d40894543524271aecde735
Carlos Alhama <patezno97@gmail.com>
2019-06-09 17:19:21 +0200

    proyecto creado

---
+++ src/main/java/org/lasencinas/cotxoxsSpring/CotxoxsSpringApplication.java
@@ -0,0 +1,13 @@
+package org.lasencinas.cotxoxsSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotxoxsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotxoxsSpringApplication.class, args);
+	}
+
+}
